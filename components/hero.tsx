8cacf79777d0138cba8f3ea94b1c297d23780f24
NoureddineElmhassani <noureddineelmhassani@gmail.com>
2025-05-02 17:14:00 +0100

    update everything

--- components/hero.tsx
+++ components/hero.tsx
@@ -131,7 +131,8 @@ export default function Hero() {
               className="inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-input bg-background hover:bg-accent hover:text-accent-foreground h-10 px-4 py-2 text-sm sm:text-lg text-primary border-white hover:bg-white/10 hover:text-white sm:px-8 sm:py-5"
             >
               <Phone className="mr-1 h-4 w-4 sm:mr-2 sm:h-5 sm:w-5" />
-              Nous appeler
+              <span className="sm:hidden">Appeler</span>
+              <span className="hidden sm:inline">Nous appeler</span>
             </a>
           </motion.div>
         </motion.div>
